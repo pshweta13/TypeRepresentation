8ba7c8694fd08d831fc48876b2eb734ac11dad7e
pshweta13 <shweta0098@gmail.com>
2024-09-08 20:25:54 +0400

    Initial Commit

---
+++ TypeRepresentation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TypeRepresentation
+//
+//  Created by Shweta Patel on 08/09/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
