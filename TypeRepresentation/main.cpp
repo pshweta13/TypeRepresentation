e36e1cc2102a083a0ddfa1d5a57f7c5b0dcb58e1
pshweta13 <shweta0098@gmail.com>
2024-09-08 20:31:29 +0400

    Second Commit

--- TypeRepresentation/main.cpp
+++ TypeRepresentation/main.cpp
@@ -3,12 +3,12 @@
 //  TypeRepresentation
 //
 //  Created by Shweta Patel on 08/09/24.
-//
+//  Examples of Type Representations
 
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "I was born in the year " << 1994 << "\n";
     return 0;
 }
